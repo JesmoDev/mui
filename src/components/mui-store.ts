a2f1d921d71f54591ac71c5ff9ee96d3924a0353
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-04-25 12:53:13 +0200

    init

---
+++ src/components/mui-store.ts
@@ -0,0 +1,166 @@
+export class Broadcaster<T> {
+  #listeners = new Set<(data: any) => void>();
+  #data: T;
+
+  constructor(data: T) {
+    this.#data = data;
+  }
+
+  subscribe(listener: (data: any) => void) {
+    this.#listeners.add(listener);
+    listener(this.#data);
+  }
+
+  unsubscribe(listener: (data: any) => void) {
+    this.#listeners.delete(listener);
+  }
+
+  get data() {
+    return this.#data;
+  }
+
+  set data(data: T) {
+    this.#data = data;
+    this.#listeners.forEach((listener) => listener(this.#data));
+  }
+}
+
+class MuiApi {
+  search(query: string) {
+    return MUIData.items.filter((item) => {
+      return item.name.toLowerCase().includes(query.toLowerCase()) || item.description.toLowerCase().includes(query.toLowerCase());
+    });
+  }
+}
+
+class MUIData {
+  static items = [
+    {
+      id: 1,
+      name: "Flameblade",
+      description: "A sword engulfed in magical flames.",
+      damage: 20,
+      damageType: "fire",
+    },
+    {
+      id: 2,
+      name: "Thunderstaff",
+      description: "A staff crackling with electric energy.",
+      damage: 12,
+      damageType: "lightning",
+    },
+    {
+      id: 3,
+      name: "Amulet of the Wind",
+      description: "An amulet that grants its wearer unparalleled speed.",
+    },
+    {
+      id: 4,
+      name: "Crystal Shield",
+      description: "A shield made of enchanted crystal.",
+      armor: 5,
+    },
+    {
+      id: 5,
+      name: "Soulstone",
+      description: "A gem that traps the souls of fallen enemies.",
+    },
+    {
+      id: 6,
+      name: "Staff of Healing",
+      description: "A staff that radiates healing energy.",
+      damage: 8,
+      damageType: "radiant",
+    },
+    {
+      id: 7,
+      name: "Shadow Cloak",
+      description: "A cloak that grants its wearer the ability to meld with shadows.",
+    },
+    {
+      id: 8,
+      name: "Frostbite Gauntlets",
+      description: "Gauntlets imbued with the power of ice.",
+      damage: 10,
+      damageType: "cold",
+    },
+    {
+      id: 9,
+      name: "Helm of Telepathy",
+      description: "A helmet that allows the wearer to read minds.",
+    },
+    {
+      id: 10,
+      name: "Serpent's Fang",
+      description: "A dagger crafted from the fang of a mythical serpent.",
+      damage: 6,
+      damageType: "poison",
+    },
+    {
+      id: 11,
+      name: "Phoenix Feather",
+      description: "A feather from the legendary phoenix, said to grant immense power.",
+    },
+    {
+      id: 12,
+      name: "Gloves of Strength",
+      description: "Gloves that increase the wearer's physical strength.",
+    },
+    {
+      id: 13,
+      name: "Ethereal Cloak",
+      description: "A cloak that allows the wearer to phase through solid objects.",
+    },
+    {
+      id: 14,
+      name: "Golem Heart",
+      description: "A magical crystal that powers constructs.",
+    },
+    {
+      id: 15,
+      name: "Aegis Plate",
+      description: "A suit of armor that grants protection from all forms of harm.",
+      armor: 10,
+    },
+    {
+      id: 16,
+      name: "Wand of Illusion",
+      description: "A wand capable of creating lifelike illusions.",
+    },
+    {
+      id: 17,
+      name: "Whispering Dagger",
+      description: "A dagger that whispers secrets to its wielder.",
+      damage: 8,
+      damageType: "psychic",
+    },
+    {
+      id: 18,
+      name: "Crown of Wisdom",
+      description: "A crown that enhances the wearer's intelligence.",
+    },
+    {
+      id: 19,
+      name: "Orcish Waraxe",
+      description: "A brutal axe wielded by orcish warriors.",
+      damage: 15,
+      damageType: "slashing",
+    },
+    {
+      id: 20,
+      name: "Spectral Chainmail",
+      description: "Chainmail woven from ethereal strands, providing ghostly protection.",
+      armor: 8,
+    },
+  ];
+}
+
+export class MUIStore {
+  public static instance: MUIStore = new MUIStore();
+
+  #api: MuiApi = new MuiApi();
+
+  search(query: string) {
+    return this.#api.search(query);
+  }
+}
---
+++ src/mui-app.element.ts
@@ -0,0 +1,22 @@
+import { css, html } from "lit";
+import { customElement } from "lit/decorators.js";
+import { MUIComponent } from "./components/mui-component";
+
+@customElement("mui-app")
+export class MuiAppElement extends MUIComponent {
+  render() {
+    return html`<h1>Hello, World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sit voluptate minima suscipit? Obcaecati, commodi iure repellendus nostrum, perspiciatis rem officia aperiam suscipit odit at
+        doloribus, doloremque repudiandae recusandae cumque.
+      </p>`;
+  }
+
+  static styles = css``;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "mui-app": MuiAppElement;
+  }
+}
